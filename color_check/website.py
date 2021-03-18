299090634a25326741cd6377a28cc5dbe6a7f6b3
Lukas Menzel <lukas.menzel@code.berlin>
2021-03-18 17:39:25 +0100

    change website.py

--- color_check/website.py
+++ color_check/website.py
@@ -5,9 +5,6 @@ import logging
 
 app = Flask(__name__)
 
-logging.basicConfig(filename='/tmp/log.txt', level=logging.DEBUG,
-                    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
-
 
 @app.route('/')
 def index():
