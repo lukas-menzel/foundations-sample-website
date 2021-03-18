918e5ff9e8b376ee783d82c30a7d4f3905a8a375
Lukas Menzel <lukas.menzel@code.berlin>
2021-03-18 17:43:28 +0100

    resolve error in test_sample_website.py

--- sample_website/tests/test_sample_website.py
+++ sample_website/tests/test_sample_website.py
@@ -2,8 +2,4 @@ from sample_website.website import hello_world
 
 
 def test_sample_website():
-
-
-<< << << < HEAD
-assert hello_world() == '<h1>Hello, Lukas!</h1>'
-== == == =
+    assert hello_world() == '<h1>Hello, Lukas!</h1>'
